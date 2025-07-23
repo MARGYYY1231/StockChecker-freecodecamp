83a9c75d431b740011ca734418d58a0e0243f391
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2025-07-23 17:50:04 +1200

    Obtains the stock/s and likes

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,16 @@
 'use strict';
 
-const bcrypt      = require('bcrypt');
-
 module.exports = function (app) {
 
+  // Main Route Handler - Handles GET requests with Query Parameters
+  // Stock: A stock symbol or array of two symbols
+  // Like: Optional Flag to like the Stock
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      const { stock, like } = req.query;
+      if(Array.isArray(stock)){
+        console.log("Stocks", stock)
+      }
     });
     
 };
