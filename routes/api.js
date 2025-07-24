d1d8aa31610cc01b377e4175f8bde6419b6861bb
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2025-07-24 15:19:45 +1200

    Added a function to get the updated stock details

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,18 @@
 const StockModel = require("../models").Stock;
 const fetch = require("node-fetch");
 
+/**
+ * Gets the Stock/s from the link
+ * https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/[symbol]/quote
+ * where symbol = msft | goog | aapl | ...
+ * @param {*} stock 
+ */
+async function aquireStock(stock) {
+  const resp = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`);
+  const {symbol, latestPrice } = await resp.json;
+  return {symbol, latestPrice};
+}
+
 module.exports = function (app) {
 
   // Main Route Handler - Handles GET requests with Query Parameters
