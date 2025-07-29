d7890393c7b7b69b06d9aa199c0d473d19dfd9e4
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2025-07-29 15:53:19 +1200

    Changed name to makeStock

--- routes/api.js
+++ routes/api.js
@@ -122,7 +122,7 @@ module.exports = function (app) {
       }
 
       //If Only one
-      const { symbol, latestPrice } = await createStock(stock);
+      const { symbol, latestPrice } = await aquireStock(stock);
       if (!symbol) {
         res.json({ stockData: { likes: like ? 1 : 0 } });
         return;
