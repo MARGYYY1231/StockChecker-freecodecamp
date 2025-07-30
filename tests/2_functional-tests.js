c2aab6957d4869ec065d6996ea1f3b8e99fa4e13
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2025-07-30 15:48:41 +1200

    Completed writing all tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -10,7 +10,7 @@ suite('Functional Tests', function() {
     //Viewing one stock
     test('Viewing one stock', function(done){
         chai.request(server)
-        .get('/api/stock-prices/')
+        .get('/api/stock-prices')
         .query({stock : 'GOLD'})
         .end((err, res) => {
             assert.equal(res.status, 200);
@@ -30,7 +30,7 @@ suite('Functional Tests', function() {
     //Viewing one stock and liking it
     test('Viewing one stock and liking it', function(done){
         chai.request(server)
-        .get('/api/stock-prices/')
+        .get('/api/stock-prices')
         .query({stock : 'GOLD', like : true})
         .end((err, res) => {
             assert.equal(res.status, 200);
@@ -52,7 +52,7 @@ suite('Functional Tests', function() {
     //Viewing one stock and liking it again
     test('Viewing one stock and liking it again', function(done){
         chai.request(server)
-        .get('/api/stock-prices/')
+        .get('/api/stock-prices')
         .query({stock : 'GOLD', like : true})
         .end((err, res) => {
             assert.equal(res.status, 200);
@@ -73,11 +73,62 @@ suite('Functional Tests', function() {
 
     //Viewing two stocks
     test('Viewing two stocks', function(done){
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: ['TSLA','AMZN']})
+        .end((err, res) => {
+            assert.equal(res.status, 200);
+            assert.isArray(res.body.stockData);
 
+            assert.property(res.body.stockData[0], 'stock');
+            assert.property(res.body.stockData[0], 'price');
+            assert.property(res.body.stockData[0], 'rel_likes');
+
+            assert.property(res.body.stockData[1], 'stock');
+            assert.property(res.body.stockData[1], 'price');
+            assert.property(res.body.stockData[1], 'rel_likes');
+
+            assert.equal(res.body.stockData[0].stock, 'TSLA');
+            assert.equal(res.body.stockData[1].stock, 'AMZN');
+
+            assert.exists(res.body.stockData[0].price, 'TSLA has a price');
+            assert.exists(res.body.stockData[1].price, 'AMZN has a price');
+
+            assert.exists(res.body.stockData[0].rel_likes, 'TSLA has rel_likes');
+            assert.exists(res.body.stockData[1].rel_likes, 'AMZN has rel_likes');
+            done();
+        });
     });
 
     //Viewing two stocks and liking them
     test('Viewing two stocks and liking them', function(done){
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: ['TSLA','AMZN'], like: true})
+        .end((err, res) => {
+            assert.equal(res.status, 200);
+            assert.isArray(res.body.stockData);
 
+            assert.property(res.body.stockData[0], 'stock');
+            assert.property(res.body.stockData[0], 'price');
+            assert.property(res.body.stockData[0], 'rel_likes');
+
+            assert.property(res.body.stockData[1], 'stock');
+            assert.property(res.body.stockData[1], 'price');
+            assert.property(res.body.stockData[1], 'rel_likes');
+
+            assert.equal(res.body.stockData[0].stock, 'TSLA');
+            assert.equal(res.body.stockData[1].stock, 'AMZN');
+
+            assert.exists(res.body.stockData[0].price, 'TSLA has a price');
+            assert.exists(res.body.stockData[1].price, 'AMZN has a price');
+
+            assert.exists(res.body.stockData[0].rel_likes, 'TSLA has rel_likes');
+            assert.exists(res.body.stockData[1].rel_likes, 'AMZN has rel_likes');
+
+            assert.isNumber(res.body.stockData[0].rel_likes);
+            assert.isNumber(res.body.stockData[1].rel_likes);
+            done();
+        });
     });
 });
