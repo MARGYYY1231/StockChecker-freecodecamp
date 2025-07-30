2726d72f694fc15f86f27bfe44638ce2595e454d
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2025-07-30 15:02:11 +1200

    Completed test to view one stock

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,44 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+    
+    //Viewing one stock
+    test('Viewing one stock', function(done){
+        chai.request(server)
+        .get('/api/stock-prices/')
+        .query({stock : 'GOLD'})
+        .end((err, res) => {
+            assert.equal(res.status, 200);
+            
+            assert.property(res.body, 'stockData');
+            assert.property(res.body.stockData, 'stock');
+            assert.property(res.body.stockData, 'price');
+            assert.property(res.body.stockData, 'likes');
 
+            assert.equal(res.body.stockData.stock, 'GOLD');
+            assert.exists(res.body.stockData.price, 'GOLD has a price.');
+
+            done();
+        });
+    });
+
+    //Viewing one stock and liking it
+    test('Viewing one stock and liking it', function(done){
+
+    });
+    
+    //Viewing one stock and liking it again
+    test('Viewing one stock and liking it again', function(done){
+
+    });
+
+    //Viewing two stocks
+    test('Viewing two stocks', function(done){
+
+    });
+
+    //Viewing two stocks and liking them
+    test('Viewing two stocks and liking them', function(done){
+
+    });
 });
